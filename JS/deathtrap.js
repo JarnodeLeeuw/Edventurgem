60533acfa303a52c7ff1fb9b0a7654f35e388c10
Jarno de Leeuw <9902732@mydavinci.nl>
2016-12-07 15:56:58 +0100

    Got a bit further. :)

--- JS/deathtrap.js
+++ JS/deathtrap.js
@@ -1,5 +1,11 @@
 function start() {
 	document.getElementById("body22").style.backgroundImage = "url(Images/room1.jpg)";
-	
-	 
+}
+
+function info() {
+	if (document.getElementById("information").style.display !="inline"){
+		document.getElementById("information").style.display="inline";
+	} else {
+		document.getElementById("information").style.display="none";
+	}
 }
