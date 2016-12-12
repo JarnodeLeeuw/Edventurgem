ac733e90d144f27efa032af618434e9626b748b4
Jarno de Leeuw <9902732@mydavinci.nl>
2016-12-12 11:41:43 +0100

    Help button works

--- JS/deathtrap.js
+++ JS/deathtrap.js
@@ -1,3 +1,5 @@
+var img = "billy";
+
 function start() {
 	document.getElementById("body22").style.backgroundImage = "url(Images/room1.jpg)";
 }
@@ -8,14 +10,11 @@ function info() {
 	} else {
 		document.getElementById("information").style.display="none";
 	}
-	if (document.getElementById("body22").style.backgroundImage = "url(Images/Billy the puppet.jpg)"){
-		document.getElementById("body22").style.backgroundImage = "url(Images/sawtv.jpg)";
+	if (img != "sawtv"){
+		document.getElementById("body22").style.backgroundImage = "url('Images/sawtv.jpg')";
+		img = "sawtv";
 	} else {
-		document.getElementById("body22").style.backgroundImage ="url(Images/sawtv.jpg)";
-	}
-	if (document.getElementById("body22").style.backgroundImage = "url(Images/sawtv.jpg)"){
-		document.getElementById("body22").style.backgroundImage = "url(Images/billy the puppet.jpg)"; 
-	} else {
-		document.getElementById("body22").style.backgroundImage = "url(Images/billy the puppet.jpg)";
+		document.getElementById("body22").style.backgroundImage ="url('Images/billy the puppet.jpg')";
+		img = "billy";
 	}
 }
