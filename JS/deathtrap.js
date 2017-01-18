51add5c4cdddc1ef157bad2610a41849d10393e7
Jarno de Leeuw <9902732@mydavinci.nl>
2017-01-18 11:56:43 +0100

    I fucked something up, now my buttons don't work...

--- JS/deathtrap.js
+++ JS/deathtrap.js
@@ -7,8 +7,30 @@ function start() {
 	document.getElementById("info").style.display ="none";
 	document.getElementById("trap1").style.display ="inline"
 	document.getElementById("key").style.display ="inline"
+	document.getElementById("timer")style.dislay ="inline"
+    setTimeout(function(){ document.getElementById("body22").style.backgroundImage = "url(Images/GAMEOVER.jpg)"; }, 3000)   
+   */ var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
 }
 
+window.onload = function () {
+    var fiveMinutes = 60 * 0.5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+}; /*
+}
 
 function info() {
 	if (document.getElementById("information").style.display !="inline"){
@@ -28,8 +50,10 @@ function info() {
 	} else {
 		document.getElementById("begin").style.display ="inline";
 	}
+	document.getElementById("audiohelp").autoplay = true;
 }
 
 function key(){
 	document.getElementById("key").style.display ="none"
-}
+	document.getElementById("pad").style.display ="inline"
+}30
